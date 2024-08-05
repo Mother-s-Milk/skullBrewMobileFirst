35b14278210f5c718eb5f8fe1fe9b118f86dbf5b
Mother-s-Milk <francoveradrms@gmail.com>
2024-08-05 11:54:36 -0300

    Más rezise con js

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let element = document.querySelector('.container');
-    let size = element.clientWidth;
-    console.log(size);
+    function setComponentWidth () {
+        let element = document.querySelector('.container');
+        let size = element.clientWidth;
+        console.log(size);
 
-    // Obtener el valor de la variable CSS --s-padding
-    let sPadding = getComputedStyle(document.documentElement).getPropertyValue('--s-padding');
+        // Obtener el valor de la variable CSS --s-padding
+        let sPadding = getComputedStyle(document.documentElement).getPropertyValue('--s-padding');
         
-    // Convertir el valor de --s-padding a número
-    sPadding = parseFloat(sPadding);
+        // Convertir el valor de --s-padding a número
+        sPadding = parseFloat(sPadding);
 
-    // Calcular el nuevo width
-    let width = (size - sPadding);
+        // Calcular el nuevo width
+        let width = (size - sPadding);
 
-    // Establecer la nueva variable CSS --s-width
-    document.documentElement.style.setProperty('--s-width', `${width}px`);
+        // Establecer la nueva variable CSS --s-width
+        document.documentElement.style.setProperty('--s-width', `${width}px`);
+    }
+    
+    setComponentWidth();
+
+    window.addEventListener('resize', setComponentWidth);
 })
